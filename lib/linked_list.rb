b7e74c26bcdb406778d15ac9774d7b2b1b6ab3e5
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-10 13:38:00 -0600

    feat: Add append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,4 +4,15 @@ class LinkedList
     def initialize(head = nil)
         @head = head
     end
+
+    def append(data)
+        new_node = Node.new(data)
+        if @head.nil?
+            @head = new_node
+        else
+            current.node = @head
+            current_node = current_node.next_node while current_node.next_node
+            current_node.next_node = new_node
+        end
+    end
 end
